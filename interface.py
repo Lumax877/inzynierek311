38be107b6f500c1e8f1134f3b8b04331bc035bae
Bartosz Ochtabiński <bochta.binski7@gmail.com>
2024-01-28 01:55:41 +0100

    added textboxes to display selected paths

--- interface.py
+++ interface.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
-from tkinter import filedialog
 from PIL import Image
 from finalscript import create_mosaic
 import os
@@ -34,7 +33,6 @@ class MosaicApp:
         self.tile_size = tk.StringVar()
         self.target_region_size = tk.StringVar()
 
-        # Wyświetlanie ścieżki dla pliku wejściowego
         self.input_path_label = ttk.Label(self.tab1, text="Input Image Path:")
         self.input_path_label.pack(pady=1)
 
@@ -45,17 +43,35 @@ class MosaicApp:
         self.input_path_entry.pack(pady=1, fill="x")
         self.input_path_scrollbar.pack(fill="x")
 
-        # Przyciski do wyboru plików
         self.input_button = tk.Button(self.tab1, text="Select Input Image", command=self.select_input_image)
         self.input_button.pack()
+
+        self.input_file_label = ttk.Label(self.tab1, text="Tile Directory Path:")
+        self.input_file_label.pack(pady=1)
+
+        self.tile_path_scrollbar = ttk.Scrollbar(self.tab1, orient="horizontal")
+        self.tile_path_entry = ttk.Entry(self.tab1, textvariable=self.tile_directory, xscrollcommand=self.tile_path_scrollbar.set)
+        self.tile_path_scrollbar.config(command=self.tile_path_entry.xview)
+
+        self.tile_path_entry.pack(pady=1, fill="x")
+        self.tile_path_scrollbar.pack(fill="x")
 
         self.tile_button = tk.Button(self.tab1, text="Select Tile Directory", command=self.select_tile_directory)
         self.tile_button.pack()
 
+        self.output_file_label = ttk.Label(self.tab1, text="Output Image Path:")
+        self.output_file_label.pack(pady=1)
+
+        self.output_path_scrollbar = ttk.Scrollbar(self.tab1, orient="horizontal")
+        self.output_path_entry = ttk.Entry(self.tab1, textvariable=self.output_image_path, xscrollcommand=self.output_path_scrollbar.set)
+        self.output_path_scrollbar.config(command=self.output_path_entry.xview)
+
+        self.output_path_entry.pack(pady=1, fill="x")
+        self.output_path_scrollbar.pack(fill="x")
+
         self.output_button = tk.Button(self.tab1, text="Select Output Image Path", command=self.select_output_image)
         self.output_button.pack()
 
-        # Pola wprowadzania dla tile_size i target_region_size
         tk.Label(self.tab1, text="Tile Size:").pack()
         self.tile_size_entry = tk.Entry(self.tab1, textvariable=self.tile_size)
         self.tile_size_entry.pack()
@@ -64,9 +80,14 @@ class MosaicApp:
         self.target_region_size_entry = tk.Entry(self.tab1, textvariable=self.target_region_size)
         self.target_region_size_entry.pack()
 
-        # Przycisk do uruchomienia skryptu
         self.run_button = tk.Button(self.tab1, text="Generate Mosaic", command=self.generate_mosaic)
         self.run_button.pack()
+
+        self.info = ttk.Label(self.tab1, text="Please use buttons instead of textboxes to select required files.")
+        self.info.pack(pady=1)
+
+        self.info2 = ttk.Label(self.tab1, text="Remember that tile directory must contain ONLY square images with same resolution.")
+        self.info2.pack(pady=1)
 
     def select_input_image(self):
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
@@ -126,11 +147,24 @@ class MosaicApp:
         self.generated_image_size_entry = tk.Entry(self.tab2, textvariable=self.generated_image_size)
         self.generated_image_size_entry.pack()
 
+        self.folder_path_label = ttk.Label(self.tab2, text="Tile Folder Path:")
+        self.folder_path_label.pack(pady=1)
+
+        self.folder_path_scrollbar = ttk.Scrollbar(self.tab2, orient="horizontal")
+        self.folder_path_entry = ttk.Entry(self.tab2, textvariable=self.generated_images_path, xscrollcommand=self.folder_path_scrollbar.set)
+        self.folder_path_scrollbar.config(command=self.folder_path_entry.xview)
+
+        self.folder_path_entry.pack(pady=1, fill="x")
+        self.folder_path_scrollbar.pack(fill="x")
+
         self.generated_images_path_button = tk.Button(self.tab2, text="Select Folder", command=self.select_generated_images_path)
         self.generated_images_path_button.pack()
 
         self.generate_images_button = tk.Button(self.tab2, text="Generate Images", command=self.generate_images)
         self.generate_images_button.pack()
+
+        self.info = ttk.Label(self.tab2, text="Warning: all files in the selected folder will be deleted when generating starts.")
+        self.info.pack(pady=1)
 
     def select_generated_images_path(self):
         folder_path = filedialog.askdirectory()
@@ -181,11 +215,24 @@ class MosaicApp:
         self.lorem_image_size_entry = tk.Entry(self.tab3, textvariable=self.lorem_image_size)
         self.lorem_image_size_entry.pack()
 
+        self.lorem_path_label = ttk.Label(self.tab3, text="Tile Folder Path:")
+        self.lorem_path_label.pack(pady=1)
+
+        self.lorem_path_scrollbar = ttk.Scrollbar(self.tab3, orient="horizontal")
+        self.lorem_path_entry = ttk.Entry(self.tab3, textvariable=self.lorem_images_path, xscrollcommand=self.lorem_path_scrollbar.set)
+        self.lorem_path_scrollbar.config(command=self.lorem_path_entry.xview)
+
+        self.lorem_path_entry.pack(pady=1, fill="x")
+        self.lorem_path_scrollbar.pack(fill="x")
+
         self.lorem_images_path_button = tk.Button(self.tab3, text="Select Folder", command=self.select_lorem_images_path)
         self.lorem_images_path_button.pack()
 
         self.lorem_images_button = tk.Button(self.tab3, text="Download Images", command=self.lorem_images)
         self.lorem_images_button.pack()
+
+        self.info = ttk.Label(self.tab3, text="Warning: all files in the selected folder will be deleted when downloading starts.")
+        self.info.pack(pady=1)
 
     def select_lorem_images_path(self):
         folder_path = filedialog.askdirectory()
@@ -207,6 +254,6 @@ class MosaicApp:
 if __name__ == "__main__":
     root = tk.Tk()
     app = MosaicApp(root)
-    root.geometry("500x300")
+    root.geometry("500x500")
     root.resizable(False, False)
     root.mainloop()
